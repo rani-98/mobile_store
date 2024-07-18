6cc575ecc0c638e54c9e7e6bd4bba55087c06624
rani-98 <nagaranianji1998@gmail.com>
2024-07-18 19:16:20 +0530

    added login

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,11 +1,7 @@
-
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 
-
-# Create your views here.
 def register(request):
     form = UserCreationForm()
     if request.method == "POST":
@@ -16,26 +12,21 @@ def register(request):
 
     return render(request, "register.html", {"form": form})
 
-
 def login_view(request):
-    # if the user is already logged in then redirect to the store page
     if request.user.is_authenticated:
+        return redirect("product")
+
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("product")
         
-
-        if request.method == "POST":
-            form = AuthenticationForm(data=request.POST)
-            if form.is_valid():
-                user = form.get_user()
-                login(request, user)
-                return redirect("product")
-        form = AuthenticationForm()
-        return render(request, "login.html", {"form": form})
-
-            
     else:
-        return redirect("customer_register")
-
-   
+        form = AuthenticationForm()
+        
+    return render(request, "login.html", {"form": form})
 
 def logout_view(request):
     logout(request)
