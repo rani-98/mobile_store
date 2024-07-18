7ef6c559b1c56d74d0ca1afe713e9b303a81d926
rani-98 <nagaranianji1998@gmail.com>
2024-07-18 17:42:58 +0530

    login page added

--- store_world/settings.py
+++ store_world/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mobile_store',
+    'user_auth',
 
 
 ]
--- store_world/urls.py
+++ store_world/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("mobile_store.urls")),
+    path('auth/',include("user_auth.urls")),
     
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
